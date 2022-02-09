83263b13fe1f807fa7612845d01fe6580c0a4e02
sosogoooood <99127970+sosogoooood@users.noreply.github.com>
2022-02-10 00:09:08 +0900

    2d context

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
-if (canvas) {
-    canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave", onMouseLeave);
-}
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
 
 function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event) {
     painting = true;
 }
 
-function onMouseUp(event) {
+function onMouseLeave(event) {
     stopPainting()
 }
 
-function onMouseLeave(event) {
-    stopPainting()
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
